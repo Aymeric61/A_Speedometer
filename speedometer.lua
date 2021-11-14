8a70219d50923a7e7af5e0c3850e795168ef1172
Aymeric61 <89253798+Aymeric61@users.noreply.github.com>
2021-11-14 14:17:22 +0100

    Add files via upload
    Speedometer tout simple tournant à 0.03 MS.
    C'est ma première création, je débute dans le développement.
    Support: https://discord.gg/cBEKgvdeMA

---
+++ speedometer.lua
@@ -0,0 +1,21 @@
+function text(content)
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.9,0.7)
+end
+
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(2)
+        
+        local speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1),false))*3.6
+        if (IsPedInAnyVehicle(GetPlayerPed(-1), false)) then 
+            text(math.floor(speed))
+        end
+    end
+
+end)
